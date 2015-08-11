18a8611375045bea0c7e01497132ba7c77e65e4c
J.S. Oishi <jsoishi@gmail.com>
2015-08-11 15:07:24 -0400

    updated to include temperature perturbation.

--- python/plot_snapshots.py
+++ python/plot_snapshots.py
@@ -18,24 +18,30 @@ import matplotlib.pyplot as plt
 
 from plot_energies import read_timeseries
 
-def create_frame(r, z, u, v, w):
+def create_frame(r, z, u, v, w, T):
     fig = plt.figure(figsize=(12,12))
-    ax = fig.add_subplot(111)
+    ax = fig.add_subplot(121)
     img = ax.pcolormesh(r, z,v,cmap='PuOr')
     img.axes.axis('image')
     ax.quiver(r, z, u, w, width=0.005)
     cb = fig.colorbar(img, pad=0.005)
     cb_ax = cb.ax
-    for a in [cb_ax, ax]:
+
+    ax2 = fig.add_subplot(122)
+    img2 = ax2.pcolormesh(r,z,T,cmap='bwr')
+    cb2 = fig.colorbar(img2, pad=0.005)
+    cb2_ax = cb2.ax
+    for a in [cb_ax, ax, ax2, cb2_ax]:
         a.tick_params(labelsize=12)
 
-    for axis in ['left','right']:
-        ax.spines[axis].set_linewidth(4.5)
+    for a in [ax,ax2]:
+        for axis in ['left','right']:
+            a.spines[axis].set_linewidth(4.5)
 
-    ax.set_xlabel(r'$r$',fontsize=24)
-    ax.set_ylabel(r'$z$',rotation='horizontal',fontsize=24,labelpad=20)
+        a.set_xlabel(r'$r$',fontsize=24)
+        a.set_ylabel(r'$z$',rotation='horizontal',fontsize=24,labelpad=20)
     cb_ax.set_ylabel(r'$v_{\theta}$',fontsize=24,rotation='horizontal',labelpad=20)
-
+    cb2_ax.set_ylabel(r'$T_1$', fontsize=24,rotation='horizontal',labelpad=20)
     return fig
 
 
@@ -81,7 +87,8 @@ if __name__ == "__main__":
             u = ts['tasks']['u'][i,:]
             v = ts['tasks']['v'][i,:]
             w = ts['tasks']['w'][i,:]
-            fig = create_frame(r,z,u,v,w)
+            T = ts['tasks']['T'][i,:]
+            fig = create_frame(r,z,u,v,w, T)
             print("saving frame {:04d}".format(count))
             filen = str(output_path / "vel_frame_{:04d}".format(count))
             fig.savefig(filen)
