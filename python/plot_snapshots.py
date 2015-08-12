49329f2a58f038cad215e006fdcae0530969f6a5
J.S. Oishi <jsoishi@gmail.com>
2015-08-12 18:10:22 -0400

    nicer frames

--- python/plot_snapshots.py
+++ python/plot_snapshots.py
@@ -18,21 +18,25 @@ import matplotlib.pyplot as plt
 
 from plot_energies import read_timeseries
 
-def create_frame(r, z, u, v, w, T):
-    fig = plt.figure(figsize=(12,12))
-    ax = fig.add_subplot(121)
+def create_frame(r, z, u, v, w, T, time):
+    fig = plt.figure(figsize=(16,12))
+    ax = fig.add_axes([0.1,0.1,0.35,0.8])
     img = ax.pcolormesh(r, z,v,cmap='PuOr')
     img.axes.axis('image')
-    ax.quiver(r, z, u, w, width=0.005)
+    ax.set_title('t = {:10.5e}'.format(time),fontsize=24)
+
+    skip = (slice(None, None, 3), slice(None, None, 3))
+    ax.quiver(r[skip[0]], z[skip[0]], u[skip], w[skip], width=0.005)
     cb = fig.colorbar(img, pad=0.005)
     cb_ax = cb.ax
 
-    ax2 = fig.add_subplot(122)
+    ax2 = fig.add_axes([0.55,0.1,0.35,0.8])
     img2 = ax2.pcolormesh(r,z,T,cmap='bwr')
+    img2.axes.axis('image')
     cb2 = fig.colorbar(img2, pad=0.005)
     cb2_ax = cb2.ax
     for a in [cb_ax, ax, ax2, cb2_ax]:
-        a.tick_params(labelsize=12)
+        a.tick_params(labelsize=16)
 
     for a in [ax,ax2]:
         for axis in ['left','right']:
@@ -82,13 +86,15 @@ if __name__ == "__main__":
 
         r = ts['scales']['r']['1.0'][:]
         z = ts['scales']['z']['1.0'][:]
+        time = ts['scales']['sim_time']
 
         for i in range(ts['tasks']['v'][:].shape[0]):
             u = ts['tasks']['u'][i,:]
             v = ts['tasks']['v'][i,:]
             w = ts['tasks']['w'][i,:]
             T = ts['tasks']['T'][i,:]
-            fig = create_frame(r,z,u,v,w, T)
+            tt = time[i]
+            fig = create_frame(r,z,u,v,w, T, tt)
             print("saving frame {:04d}".format(count))
             filen = str(output_path / "vel_frame_{:04d}".format(count))
             fig.savefig(filen)
